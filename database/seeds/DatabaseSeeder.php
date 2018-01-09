2d6a5ec376472a36bddef6d97a794a0da9165020
thuongnvsoict <khoithuongcorporation@gmail.com>
2018-01-09 09:35:28 +0700

    first-time

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+    }
+}
+class userSeedeer extends Seeder{
+    public function run(){
+        DB::table('user')->insert([
+            'name'=>'Nguyễn Văn Thưởng',
+            'phonenumber' => '01667958012',
+            'email' => 'thuongnv.soict@gmail.com',
+            'address' => 'Ha Noi',
+            'password' => bcrypt('12345678'),
+            'secret' => 'boss'
+        ]);
     }
 }
